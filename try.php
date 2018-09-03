857440831b564d100af3a60efafdca42fc787b07
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-04 03:59:15 +0800

    Update try.php

--- try.php
+++ try.php
@@ -1,1 +1,46 @@
+<?php
+require  'vendor/autoload.php';
+date_default_timezone_set('Asia/Taipei');
+use Google\Spreadsheet\DefaultServiceRequest;
+use Google\Spreadsheet\ServiceRequestFactory;
+putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/My Project-aeb1d8a3a4ed.json');
+		/*  SEND TO GOOGLE SHEETS */
+		 $client = new Google_Client;
+			try{
+				$client->useApplicationDefaultCredentials();
+			  $client->setApplicationName("Something to do with my representatives");
+				$client->setScopes(['https://www.googleapis.com/auth/drive','https://spreadsheets.google.com/feeds']);
+			   if ($client->isAccessTokenExpired()) {
+					$client->refreshTokenWithAssertion();
+				}
+
+				$accessToken = $client->fetchAccessTokenWithAssertion()["access_token"];
+				ServiceRequestFactory::setInstance(
+					new DefaultServiceRequest($accessToken)
+				);
+			   // Get our spreadsheet
+				$spreadsheet = (new Google\Spreadsheet\SpreadsheetService)
+					->getSpreadsheetFeed()
+					->getByTitle('test9999');
+
+				// Get the first worksheet (tab)
+				$worksheets = $spreadsheet->getWorksheetFeed()->getEntries();
+				$worksheet = $worksheets[0];
+
+
+				$listFeed = $worksheet->getListFeed();
+				$listFeed->insert([
+					'name' => "'". 'Igor',
+					'phone' => "'". '2425-245-224545',
+					'surname' => "'". 'Orlov',
+					'city' => "'". 'Berlin',
+					'age' => "'". '35',
+					'date' => date_create('now')->format('Y-m-d H:i:s')
+				]);
+
+			}catch(Exception $e){
+			  echo $e->getMessage() . ' ' . $e->getLine() . ' ' . $e->getFile() . ' ' . $e->getCode;
+			}
+			
+			/*  SEND TO GOOGLE SHEETS */
 
