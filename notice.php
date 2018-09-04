a1e81146330b4eb193099fc05c356f661dd7122a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-04 21:36:04 +0800

    Update notice.php

--- notice.php
+++ notice.php
@@ -12,6 +12,7 @@ switch ($event['type']) {
     case 'message':
 	$message = $event['message'];
 	$keywords = explode('-', $message['text']);
+  	$key = array_shift($keywords);
 }
 }
 putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/My Project-aeb1d8a3a4ed.json');
@@ -38,7 +39,7 @@ putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/My Project-aeb1d8a3a4ed.j
 				$listFeed = $worksheet->getListFeed();
 				$listFeed->insert([
 					'name' => "'". $userName,
-					'message' => "'". $keywords[1],
+					'message' => "'". $key,
 					'title' => "'". '公告',
 					'date' => date_create('now')->format('Y-m-d H:i:s')
 				]);
