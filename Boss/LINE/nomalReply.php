61c155165a13c4046b1d00734368f297c178f7b1
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-05 03:04:22 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -99,7 +99,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
     
 		if (count($rplyArr) == 1) {return buildTextMessage(''.$userName.'，你到底想讓我做啥?');}
 		require_once('./item1.php');
-		return buildTextMessage(''.$result.'');
+		return buildTextMessage(''.$result1.'');
 	}
 	
           
