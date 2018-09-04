ac30e39875802df439c3af6215843c6dd12d4d0b
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-04 22:24:17 +0800

    Update notice.php

--- notice.php
+++ notice.php
@@ -12,7 +12,7 @@ switch ($event['type']) {
     case 'message':
 	$message = $event['message'];
 	$keywords = explode(' ', $message['text']);
-	
+	$key = substr($keywords,1,-1,"utf-8")
 }
 }
 putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/My Project-aeb1d8a3a4ed.json');
