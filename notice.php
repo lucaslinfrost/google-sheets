5d517e8393b8e83e7169cf8a76a8c4a6d0dd89c0
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-04 21:37:43 +0800

    Update notice.php

--- notice.php
+++ notice.php
@@ -39,7 +39,7 @@ putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/My Project-aeb1d8a3a4ed.j
 				$listFeed = $worksheet->getListFeed();
 				$listFeed->insert([
 					'name' => "'". $userName,
-					'message' => "'". $key,
+					'message' => "'". $keywords,
 					'title' => "'". '公告',
 					'date' => date_create('now')->format('Y-m-d H:i:s')
 				]);
