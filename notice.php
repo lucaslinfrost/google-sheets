4ae3af12f834e72a48b3c8cece7190064c368647
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-04 20:59:06 +0800

    Update notice.php

--- notice.php
+++ notice.php
@@ -38,6 +38,7 @@ putenv('GOOGLE_APPLICATION_CREDENTIALS=' . __DIR__ . '/My Project-aeb1d8a3a4ed.j
 				$listFeed->insert([
 					'name' => "'". $userName,
 					'message' => "'". $message['text'],
+					'title' => "'". "公告",
 					'date' => date_create('now')->format('Y-m-d H:i:s')
 				]);
 			}catch(Exception $e){
