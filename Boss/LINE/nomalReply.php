65e4ab293b4f9757488741ddeb565465f81f9eb1
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-05 22:18:29 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -78,7 +78,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
     
 		if (count($rplyArr) == 1) {return buildTextMessage(''.$userName.'，你到底想讓我做啥?');}
 		require_once('./bot2.php');
-		return buildTextMessage(''.$store_text1.'');
+		return buildTextMessage(''.$code.'');
 	}
 	//查生產(文字)
 	if(stristr($inputStr, 'p') != false||
