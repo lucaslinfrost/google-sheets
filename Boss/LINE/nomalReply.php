25afd1fe17f9cf15c1f945a2889e880c081c18b6
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-07 01:22:01 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -48,11 +48,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 		}
 	}	
 	}
-	
-	//test
-	if(stristr($inputStr, '公告') != false) {
-	$bot->leave_room($roomId);
-	}
+
 
 	//公告
 	//可以是為一個使用外聯檔案的範例
