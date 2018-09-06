8e26b775a47e0248ae196a76296d1d7e8e25dac4
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-07 00:14:11 +0800

    Update index.php

--- Boss/LINE/index.php
+++ Boss/LINE/index.php
@@ -45,20 +45,6 @@ function buildTextMessage($inputStr){
 	return $message;
 }
 
-//建立陣列文字訊息的函數
-function buildArrayTextMessage($inputStr){
-	settype($inputStr, "string");
-	error_log("訊息【".$inputStr."】準備以文字訊息回傳");
-	$bot->replyMessage(
-	array(
-	'replyToken' => $event['replyToken'],
-	'messages' => $inputStr
-	)
-	);
-	return $message;
-}
-
-
 //建立圖片訊息的函數
 function buildImgMessage($inputStr){	
 	settype($inputStr, "string");
