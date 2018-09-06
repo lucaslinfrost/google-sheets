b64c5632b843447b27ed12ea4f7cf3b56ae6bce4
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-07 01:26:20 +0800

    Update LINEBotTiny.php

--- Boss/LINE/LINEBotTiny.php
+++ Boss/LINE/LINEBotTiny.php
@@ -220,4 +220,50 @@ class LINEBotTiny
         return $signature;
     }
 
+	
+	
+public function leaveRoom($roomId)
+{
+    $header = array(
+        'Authorization: Bearer ' . $this->channelAccessToken,
+    );
+
+    $context = stream_context_create(array(
+        "http" => array(
+            "method" => "POST",
+            "header" => implode("\r\n", $header),
+            "content" => "[]",
+        ),
+    ));
+
+    $response = file_get_contents('https://api.line.me/v2/bot/room/'.urlencode($roomId).'/leave', false, $context);
+    if (strpos($http_response_header[0], '200') === false) {
+        http_response_code(500);
+        error_log("Request failed: " . $response);
+    }
+}
+
+public function leaveGroup($groupId)
+{
+    $header = array(
+        'Authorization: Bearer ' . $this->channelAccessToken,
+    );
+
+    $context = stream_context_create(array(
+        "http" => array(
+            "method" => "POST",
+            "header" => implode("\r\n", $header),
+            "content" => "[]",
+        ),
+    ));
+
+    $response = file_get_contents('https://api.line.me/v2/bot/group/'.urlencode($groupId).'/leave', false, $context);
+    if (strpos($http_response_header[0], '200') === false) {
+        http_response_code(500);
+        error_log("Request failed: " . $response);
+    }
+}	
+	
+	
+	
 }
