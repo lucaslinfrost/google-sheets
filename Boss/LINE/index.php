3d9c8248aeca26281c7b29bbf207fd1d1c0fa1c0
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-06 23:33:37 +0800

    Update index.php

--- Boss/LINE/index.php
+++ Boss/LINE/index.php
@@ -50,11 +50,10 @@ function buildArrayTextMessage($inputStr){
 	error_log("訊息【".$inputStr."】準備以文字訊息回傳");
 	switch ($message['type']) {
 		case 'text':
-		require_once('./item1.php');
-		$result = array_slice($result,-1,1); 
+		$inputStr = array_slice($inputStr,-1,1); 
                     $bot->replyMessage(array(
                         'replyToken' => $event['replyToken'],
-                        'messages' => $result,
+                        'messages' => $inputStr,
                     ));
 		break;
 }
