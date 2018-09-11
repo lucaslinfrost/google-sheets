20a28854c4bb299b28cfe1c956f5d1ff7c16737d
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-12 03:26:02 +0800

    Update index.php

--- Boss/LINE/index.php
+++ Boss/LINE/index.php
@@ -316,12 +316,12 @@ function create($inputStr,$userName){
 	
 	return buildTextMessage(
 		"
-【名字】：".$userName."
-【職業】：".$bg["PD"][Dice(count($bg["PD"]))-1]."的".$bg["IB"][Dice(count($bg["IB"]))-1]."。
-【稱號】：".$bg["SP"][Dice(count($bg["SP"]))-1]."之".$bg["SPW"][Dice(count($bg["SPW"]))-1]."。
-【擅長】：對他而言，最重要的地點是".$bg["ML"][Dice(count($bg["ML"]))-1]."。
-【喜歡】：他最寶貴的東西就是".$bg["TP"][Dice(count($bg["TP"]))-1]."。
-【特徵】：總括來說，".$userName."是一個".$bg["T"][Dice(count($bg["T"]))-1]."。"
+【名字】 ： ".$userName."
+【職業】 ： ".$bg["PD"][Dice(count($bg["PD"]))-1]."的".$bg["IB"][Dice(count($bg["IB"]))-1]."。
+【稱號】 ： ".$bg["SP"][Dice(count($bg["SP"]))-1]."之".$bg["SPW"][Dice(count($bg["SPW"]))-1]."。
+【擅長】 ： ".$bg["ML"][Dice(count($bg["ML"]))-1]."。
+【喜歡】 ： 他最寶貴的東西就是".$bg["TP"][Dice(count($bg["TP"]))-1]."。
+【特徵】 ： 總括來說，".$userName."是一個".$bg["T"][Dice(count($bg["T"]))-1]."。"
 		);
 	
 }
