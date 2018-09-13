92ca2140c78c1a4043a9a7b4b60c6d025448c94a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-14 02:03:46 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -21,58 +21,58 @@ foreach ($client->parseEvents() as $event) {
                 $keywords = explode(',', $item['gsx$keyword']['$t']);
                 foreach ($keywords as $keyword) {
                     if (strcmp($code[1], $keyword) === 0) {
-if($item['gsx$others'] == ""){
+if($item['gsx$others'] === ""){
 $a = "";
 }else{
 $a = "
 其他生產要求 : ".$item['gsx$others']['$t'];
 }
 
-if($item['gsx$item2'] == ""){
+if($item['gsx$item2'] === ""){
 $b = "";
 }else{                        
 $b = "
 材料2 : ".$item['gsx$item2']['$t'];
 }
 
-if($item['gsx$item3'] == ""){
+if($item['gsx$item3'] === ""){
 $c = "";
 }else{
 $c = "
 材料3 : ".$item['gsx$item3']['$t'];
 }
 
-if($item['gsx$item4'] == ""){
+if($item['gsx$item4'] === ""){
 $d = "";
 }else{
 $d = "
 材料4 : ".$item['gsx$item4']['$t'];
 }
 
-if($item['gsx$sua'] == ""){
+if($item['gsx$sua'] === ""){
 $e = "";
 }else{
 $e = "
 大成功A : ".$item['gsx$sua']['$t'];
 }
 
-if($item['gsx$sub'] == ""){
+if($item['gsx$sub'] === ""){
 $f = "";
 }else{
 $f = "
 大成功B : ".$item['gsx$sub']['$t'];
 }
 
-if($item['gsx$remark'] == ""){
+if($item['gsx$remark'] === ""){
 $g = "";
 }else{
 $g = "
 備註 :\n".$item['gsx$remark']['$t'];
 }
                     
-                    $alltext = $alltext."".$item['gsx$pname']['$t']."\n
-                    生產等級 : ".$item['gsx$newlv']['$t']."".$a."
-                    材料1 : ".$item['gsx$item1']['$t']."".$b."".$c."".$d."".$e."".$f."".$g;
+$alltext = $alltext."".$item['gsx$pname']['$t']."\n
+生產等級 : ".$item['gsx$newlv']['$t']."".$a."
+材料1 : ".$item['gsx$item1']['$t']."".$b."".$c."".$d."".$e."".$f."".$g;
                     $alltext = $alltext."".$mline;
                     }
                 }
