a2d626253692c144c4f411b1a5e2a04bd76038e2
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-16 07:27:48 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -4,13 +4,13 @@ require_once('./LINEBotTiny.php');
 require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-$googledataspi2 = getenv('googledataspi2');
+$googledataspi = getenv('googledataspi6');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            $json = file_get_contents($googledataspi2);
+            $json = file_get_contents($googledataspi);
             $data = json_decode($json, true);
             $c = new utf8_chinese;
             $message['text'] = $c->gb2312_big5($message['text']);
