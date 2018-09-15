3b7ef4d8cb757cbb2e8ce53053aa5b600872cd0d
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-16 07:11:38 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -1,5 +1,5 @@
 <?php
-//生產頁(文字介面)
+//裝備查詢頁(文字介面)
 require_once('./LINEBotTiny.php');
 require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
@@ -17,9 +17,8 @@ foreach ($client->parseEvents() as $event) {
             $code = explode(' ', $message['text']);
             $alltext = "";
             $mline = "\n--------  分°Д°行  --------\n";
-            $blank = "";
             foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode(',', $item['gsx$keyword']['$t']);
+                $keywords = explode(',', $item['gsx$key']['$t']);
                 foreach ($keywords as $keyword) {
                     if (strcmp($code[1], $keyword) === 0) {
 if($item['gsx$others']['$t'] === ""){
@@ -28,42 +27,36 @@ $a = "";
 $a = "
 其他生產要求 : ".$item['gsx$others']['$t'];
 }
-
 if($item['gsx$item2']['$t'] === ""){
 $b = "";
 }else{                        
 $b = "
 材料2 : ".$item['gsx$item2']['$t'];
 }
-
 if($item['gsx$item3']['$t'] === ""){
 $c = "";
 }else{
 $c = "
 材料3 : ".$item['gsx$item3']['$t'];
 }
-
 if($item['gsx$item4']['$t'] === ""){
 $d = "";
 }else{
 $d = "
 材料4 : ".$item['gsx$item4']['$t'];
 }
-
 if($item['gsx$sua']['$t'] === ""){
 $e = "";
 }else{
 $e = "
 大成功A : ".$item['gsx$sua']['$t'];
 }
-
 if($item['gsx$sub']['$t'] === ""){
 $f = "";
 }else{
 $f = "
 大成功B : ".$item['gsx$sub']['$t'];
 }
-
 if($item['gsx$remark']['$t'] === ""){
 $g = "";
 }else{
