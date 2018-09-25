4b7ddd4f886991ea29a4a166e16b30b0f0077e92
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-09-26 00:22:51 +0800

    Update star.php

--- Boss/LINE/star.php
+++ Boss/LINE/star.php
@@ -22,8 +22,7 @@ foreach ($client->parseEvents() as $event) {
                 $keywords = explode(',', $item['gsx$keyword']['$t']);
                 foreach ($keywords as $keyword) {
                     if (strcmp($code[1], $keyword) === 0) {
-                        $key = explode(',', $item['gsx$key']['$t']);
-                        if (strcmp($code[2], $key) === 0) {
+                        if (strpos($code[2], $item['gsx$key']['$t']) !== false) {
                         $candidate = array(
                             'thumbnailImageUrl' => 'https://imgur.com/KQsuipD.png',
                             'title' => $item['gsx$name']['$t'],
