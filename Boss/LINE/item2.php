0895c1024da198c4fa1de2a711a3a0ca03cd6d3f
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-10-12 23:49:57 +0800

    Update item2.php

--- Boss/LINE/item2.php
+++ Boss/LINE/item2.php
@@ -14,59 +14,16 @@ foreach ($client->parseEvents() as $event) {
             $data = json_decode($json, true);
             $c = new utf8_chinese;
             $message['text'] = $c->gb2312_big5($message['text']);
-            $code = explode(' ', $message['text']);
+            $code = explode('#', $message['text']);
             $alltext = "";
             $mline = "\n--------  分°Д°行  --------\n";
             foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode(',', $item['gsx$keyword']['$t']);
+                $keywords = explode(',', $item['gsx$ppic']['$t']);
                 foreach ($keywords as $keyword) {
                     if (strcmp($code[1], $keyword) === 0) {
-if($item['gsx$others']['$t'] === ""){
-$a = "";
-}else{
-$a = "
-其他生產要求 : ".$item['gsx$others']['$t'];
-}
-if($item['gsx$item2']['$t'] === ""){
-$b = "";
-}else{                        
-$b = "
-材料2 : ".$item['gsx$item2']['$t'];
-}
-if($item['gsx$item3']['$t'] === ""){
-$c = "";
-}else{
-$c = "
-材料3 : ".$item['gsx$item3']['$t'];
-}
-if($item['gsx$item4']['$t'] === ""){
-$d = "";
-}else{
-$d = "
-材料4 : ".$item['gsx$item4']['$t'];
-}
-if($item['gsx$sua']['$t'] === ""){
-$e = "";
-}else{
-$e = "
-大成功A : ".$item['gsx$sua']['$t'];
-}
-if($item['gsx$sub']['$t'] === ""){
-$f = "";
-}else{
-$f = "
-大成功B : ".$item['gsx$sub']['$t'];
-}
-if($item['gsx$remark']['$t'] === ""){
-$g = "";
-}else{
-$g = "
-備註 :\n".$item['gsx$remark']['$t'];
-}
                     
 $alltext = $alltext."".$item['gsx$pname']['$t']."\n
-生產等級 : ".$item['gsx$newlv']['$t']."".$a."
-材料1 : ".$item['gsx$item1']['$t']."".$b."".$c."".$d."".$e."".$f."".$g;
+生產等級 : ".$item['gsx$newlv']['$t'];
                     $alltext = $alltext."".$mline;
                     }
                 }
