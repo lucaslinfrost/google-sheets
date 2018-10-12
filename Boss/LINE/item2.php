4e762addc9148900e7ed56fce9346ddb013fe3ba
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-10-12 22:58:29 +0800

    Update item2.php

--- Boss/LINE/item2.php
+++ Boss/LINE/item2.php
@@ -1,58 +1,75 @@
 <?php
-
-//掉落物品搜尋介面(旋轉木馬試做版)
-
+//生產頁(素材搜索介面)
 require_once('./LINEBotTiny.php');
+require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-$googledataspi = getenv('googledataspi');
+$googledataspi2 = getenv('googledataspi2');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            $json = file_get_contents($googledataspi);
+            $json = file_get_contents($googledataspi2);
             $data = json_decode($json, true);
-            $result = array();
+            $c = new utf8_chinese;
+            $message['text'] = $c->gb2312_big5($message['text']);
+            $code = explode(' ', $message['text']);
+            $alltext = "";
+            $mline = "\n--------  分°Д°行  --------\n";
             foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode('、', $item['gsx$key']['$t']);
+                $keywords = explode(',', $item['gsx$keyword']['$t']);
                 foreach ($keywords as $keyword) {
-                    if (strpos($message['text'], $keyword) !== false) {
-                        $candidate = array(
-                            'thumbnailImageUrl' => 'https://imgur.com/KQsuipD.png',
-                            'title' => $item['gsx$name']['$t'],
-                            'text' => $item['gsx$type']['$t']."等級".$item['gsx$level']['$t'],
-                            'actions' => array(
-                                array(
-                                    'type' => 'message',
-                                    'label' => '詳細資訊',
-                                    'text' => "怪物名稱 : ".$item['gsx$name']['$t']."\n等級 : ".$item['gsx$level']['$t']."\n--------  出沒地圖  --------\n".$item['gsx$map']['$t']."\n--------  掉落資訊  --------\n".$item['gsx$drop1']['$t']."\n".$item['gsx$drop2']['$t']."\n".$item['gsx$drop3']['$t']."\n".$item['gsx$drop4']['$t'],
-                                    )
-                                ),
-                            );
-                        array_push($result, $candidate);
+                    if (strcmp($code[1], $keyword) === 0) {
+if($item['gsx$others']['$t'] === ""){
+$a = "";
+}else{
+$a = "
+其他生產要求 : ".$item['gsx$others']['$t'];
+}
+if($item['gsx$item2']['$t'] === ""){
+$b = "";
+}else{                        
+$b = "
+材料2 : ".$item['gsx$item2']['$t'];
+}
+if($item['gsx$item3']['$t'] === ""){
+$c = "";
+}else{
+$c = "
+材料3 : ".$item['gsx$item3']['$t'];
+}
+if($item['gsx$item4']['$t'] === ""){
+$d = "";
+}else{
+$d = "
+材料4 : ".$item['gsx$item4']['$t'];
+}
+if($item['gsx$sua']['$t'] === ""){
+$e = "";
+}else{
+$e = "
+大成功A : ".$item['gsx$sua']['$t'];
+}
+if($item['gsx$sub']['$t'] === ""){
+$f = "";
+}else{
+$f = "
+大成功B : ".$item['gsx$sub']['$t'];
+}
+if($item['gsx$remark']['$t'] === ""){
+$g = "";
+}else{
+$g = "
+備註 :\n".$item['gsx$remark']['$t'];
+}
+                    
+$alltext = $alltext."".$item['gsx$pname']['$t']."\n
+生產等級 : ".$item['gsx$newlv']['$t']."".$a."
+材料1 : ".$item['gsx$item1']['$t']."".$b."".$c."".$d."".$e."".$f."".$g;
+                    $alltext = $alltext."".$mline;
                     }
                 }
-            }
-            switch ($message['type']) {
-                case 'text':
-                    $client->replyMessage(array(
-                        'replyToken' => $event['replyToken'],
-                        'messages' => array(
-                            array(
-                                'type' => 'template',
-                                'altText' => '關於 '.$message['text'].' 的資料',
-                                'template' => array(
-                                    'type' => 'carousel',
-                                    'columns' => $result,
-                                )
-                            ),
-                        ),
-                    ));
-                    break;
-                default:
-                    error_log("Unsupporeted message type: " . $message['type']);
-                    break;
             }
             break;
         default:
