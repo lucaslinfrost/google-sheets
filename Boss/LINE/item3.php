ac70acd156f4d65366bdd2876b908d69b9f1ca49
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-10-24 05:59:28 +0800

    Update item3.php

--- Boss/LINE/item3.php
+++ Boss/LINE/item3.php
@@ -4,24 +4,24 @@ require_once('./LINEBotTiny.php');
 require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
-$googledataspi2 = getenv('googledataspi6');
+$googledataspi = getenv('googledataspi6');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            $json = file_get_contents($googledataspi2);
-            $data = json_decode($json, true);
+            $json2 = file_get_contents($googledataspi);
+            $data2 = json_decode($json2, true);
             $c = new utf8_chinese;
             $message['text'] = $c->gb2312_big5($message['text']);
             $code = explode('#', $message['text']);
             $alltext2 = "";
-            foreach ($data['feed']['entry'] as $item) {
-                $keywords = explode(',', $item['gsx$search']['$t']);
-                foreach ($keywords as $keyword) {
-                    if (strcmp($code[1], $keyword) === 0) {
+            foreach ($data2['feed']['entry'] as $item2) {
+                $keywords2 = explode(',', $item2['gsx$search']['$t']);
+                foreach ($keywords2 as $keyword2) {
+                    if (strcmp($code[1], $keyword2) === 0) {
                     
-$alltext2 = $alltext2."".$item['gsx$name']['$t']." → ".$item['gsx$smith']['$t']."強化\n";
+$alltext2 = $alltext2."".$item2['gsx$name']['$t']." → ".$item2['gsx$smith']['$t']."強化\n";
                     }
                 }
             }
