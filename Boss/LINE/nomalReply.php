1c8d3db9127a2bdcfbeb2aecbf5562a1fda29b7f
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-10-24 06:22:42 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -244,15 +244,14 @@ return buildTextMessage(''.$userName.'，你沒有使用老大的權限!
 		$alltext = substr($alltext, 0, -1);
 		return buildTextMessage(''.$alltext.'');
 		}
-		}else{
-		if ($alltext2 === "") {
+		}
+		if ($alltext === "") {
 		$rplyArr = Array(
                  '你眼睛業障重ಠ_ಠ所以看不到',
                  '我找不到(๑•́ ₃ •̀๑)',
                  '資料庫沒有你要找的資料ʅ（´◔౪◔）ʃ',
                  '沒有喵(=ↀωↀ=)');
        		return buildTextMessage(''.$userName.'，'.$rplyArr[Dice(count($rplyArr))-1].'');
-		}	
 		}
 	}
 	
