8f9cf0f29a9a04bf7de520d02cdf8088f718a461
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-04 10:23:35 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -276,22 +276,17 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 	}
 	//採礦(文字)
 	if(stristr($inputStr, '挖') != false) {
-		
+		$main = "《採礦功能一覽》\n\n採礦需要工具[鶴嘴鋤]\n相關裝備 : 礦工的手帕\n\n--------  採礦列表  --------\n石塊\n銅礦\n鋅礦\n秘銀\n金礦\n銀礦\n白金礦\n秘銀礦\n赤鐵礦\n冰之碎片\n白色冰棒\n石頭碎片\n紅色原石\n藍色原石\n黃色原石\n綠色原石\n白色原石\n黑色原石\n堅硬的石頭\n大馬士革鋼";
 		$rplyArr = explode(' ',$inputStr);
     
-		if (count($rplyArr) == 1) {return buildTextMessage(''.$userName.'，你到底想讓我做啥?');}
+		if (count($rplyArr) == 1) {return buildTextMessage(''.$main.'');}
 		require_once('./drill.php');
 		if ($alltext !== "") {
 		$alltext = substr($alltext, 0, -34);
 		return buildTextMessage(''.$alltext.'');
 		}
 		if ($alltext === "") {
-		$rplyArr = Array(
-                 '你眼睛業障重ಠ_ಠ所以看不到',
-                 '我找不到(๑•́ ₃ •̀๑)',
-                 '資料庫沒有你要找的資料ʅ（´◔౪◔）ʃ',
-                 '沒有喵(=ↀωↀ=)');
-       		return buildTextMessage(''.$userName.'，'.$rplyArr[Dice(count($rplyArr))-1].'');
+       		return buildTextMessage(''.$main.'');
 		}
 	 }
 	
