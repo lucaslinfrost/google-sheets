a10e0276431bb891d66cfd8def6ef6255ad05d41
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-13 00:40:00 +0800

    Update map.php

--- Boss/LINE/map.php
+++ Boss/LINE/map.php
@@ -14,91 +14,78 @@ switch ($event['type']) {
         $c = new utf8_chinese;
         $message['text'] = $c->gb2312_big5($message['text']);
         $code = explode(' ', $message['text']);
-$graph = array(
-  'A' => array('B' => 3, 'D' => 3, 'F' => 6),
-  'B' => array('A' => 3, 'D' => 1, 'E' => 3),
-  'C' => array('E' => 2, 'F' => 3),
-  'D' => array('A' => 3, 'B' => 1, 'E' => 1, 'F' => 2),
-  'E' => array('B' => 3, 'C' => 2, 'D' => 1, 'F' => 5),
-  'F' => array('A' => 6, 'C' => 3, 'D' => 2, 'E' => 5),
-);
-$g = new Dijkstra($graph);
-$g->shortestPath('D', 'C');  // 3:D->E->C
 
 }
 };
 
-class PriorityQueue extends SplPriorityQueue
-{
-    public function compare($p1, $p2) {
-        if ($p1 == $p2) {
-			return 0;
-		}
-		else {
-            return ($p1 < $p2) ? 1 : -1;
-		}
-  }
-}
-class Dijkstra
+class Graph
 {
     protected $graph;
+    protected $visited = array();
     public function __construct($graph) {
         $this->graph = $graph;
     }
-    public function shortestPath($source, $target) {
-        // initialize Q, d and pi for all vertices
-        $d = array();  // array of best estimates of shortest path to each vertex
-        $pi = array(); // array of predecessors for each vertex
-        $Q = new SplPriorityQueue(); // queue of all unoptimized vertices
-        foreach ($this->graph as $v => $adj) {
-            $d[$v] = INF; // set initial distance to "infinity"
-            $pi[$v] = null; // no known predecessors yet
-            foreach ($adj as $w => $cost) {
-                // use the edge cost as the priority
-                $Q->insert($w, $cost);
-            }
+    public function leastHops($origin, $destination) {
+        // mark all nodes as unvisited
+        foreach ($this->graph as $key => $vertex) {
+            $this->visited[$key] = false;
         }
-        // initial distance at source is 0
-        $d[$source] = 0;
-        while (!$Q->isEmpty()) {
-            // extract min cost
-            $u = $Q->extract();
-            if (!empty($this->graph[$u])) {
-                // "relax" each adjacent vertex
-                foreach ($this->graph[$u] as $v => $cost) {
-                    // alternate route length to adjacent neighbor
-                    $alt = $d[$u] + $cost;
-                    // if alternate route is shorter
-                    if ($alt < $d[$v]) {
-                        $d[$v] = $alt; // update minimum length to vertex
-                        $pi[$v] = $u;  // add neighbor to predecessors for vertex
+        // create an empty queue
+        $q = new SplQueue();
+      
+        // enqueue the origin vertex and mark as visited
+        $q->enqueue($origin);
+        $this->visited[$origin] = true;
+      
+        // an array stack to track the path back from each node
+        $path = array();
+        $path[$origin] = new SplDoublyLinkedList();
+        $path[$origin]->setIteratorMode(
+            SplDoublyLinkedList::IT_MODE_FIFO | SplDoublyLinkedList::IT_MODE_KEEP
+        );
+      
+        $path[$origin]->push($origin);
+      
+        $found = false;
+        // while queue is not empty and destination not found
+        while (!$q->isEmpty() && $q->bottom() != $destination) {
+            $t = $q->dequeue();
+      
+            if (!empty($this->graph[$t])) {
+                // for each adjacent neighbor
+                foreach ($this->graph[$t] as $vertex) {
+                    if (!$this->visited[$vertex]) {
+                        // if not yet visited, enqueue vertex and mark as visited
+                        $q->enqueue($vertex);
+                        $this->visited[$vertex] = true;
+                        // add vertex to current node path
+                        $path[$vertex] = clone $path[$t];
+                        $path[$vertex]->push($vertex);
                     }
                 }
             }
         }
-        // we can now find the shortest path using reverse iteration
-        $S = new SplStack(); // construct the shortest path with a stack S
-        $u = $target;
-        $dist = 0;
-        // traverse from target to source
-        while (isset($pi[$u]) && $pi[$u]) {
-            $S->push($u);
-            $dist += $this->graph[$u][$pi[$u]];  // add distance to next predecessor
-            $u = $pi[$u];
-        }
-        // stack will be empty if there is no route back
-        if ($S->isEmpty()) {
-            $alltext = "No route from $source to $target\n";
+      
+        if (isset($path[$destination])) {
+	count($path[$destination]) - 1;
+            $alltext = "$origin to $destination in ";
+            $sep = '\n->';
+            foreach ($path[$destination] as $vertex) {
+                $alltext = $vertex."".$sep;
+            }
         }
         else {
-            // add the source node and print the path in reverse (LIFO) order
-            $S->push($source);
-            $totalmap = "$dist個地圖";
-            $sep = '\n->';
-            foreach ($S as $v) {
-                $alltext = $alltext."".$sep."".$v;
-            }
-          
+            $alltext =  "No route from $origin to $destination";
         }
     }
 }
+$graph = array(
+  'A' => array('B', 'F'),
+  'B' => array('A', 'D', 'E'),
+  'C' => array('F'),
+  'D' => array('B', 'E'),
+  'E' => array('B', 'D', 'F'),
+  'F' => array('A', 'E', 'C'),
+);
+$g = new Graph($graph);
+$g->leastHops('$code[1]', '$code[2]');
