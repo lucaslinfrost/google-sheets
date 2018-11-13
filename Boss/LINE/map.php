22c9901e1b65aebf955d08b069efa2bb3cd6e50b
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-14 00:04:46 +0800

    Update map.php

--- Boss/LINE/map.php
+++ Boss/LINE/map.php
@@ -17,7 +17,8 @@ foreach ($client->parseEvents() as $event) {
   '洛恩法山脈' => array('洛庫庫街','洛庫庫礦山之村','洛恩法洞窟','洛庫庫風洞'),
 );
           $g = new Graph($graph);
-          $g->leastHops('拜倫陣地', '洛恩法山');
+          $g->leastHops($code[1], $code[2]);
+          error_log("從[".$code[1]."] 到 [".$code[2]."]");
         break;
     default:
         error_log("Unsupporeted event type: " . $event['type']);
