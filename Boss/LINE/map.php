24ff359b004860de62528d05d3ee5f7ea2cc1281
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-14 01:22:35 +0800

    Update map.php

--- Boss/LINE/map.php
+++ Boss/LINE/map.php
@@ -94,9 +94,8 @@ $sep = "
 $maphop = "沒有從【".$origin."】
 到【".$destination."】的路。";
         }
-        
-    }
-    $maphop = substr($maphop, 0, -3);
+        $maphop = substr($maphop, 0, -3);
         $maphop = $title."".$maphop;
         return buildTextMessage(''.$maphop.'');
+    }
 }
