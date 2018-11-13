fc1b2f1820ecc34beb098e75de46ebeb15721519
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-13 23:26:38 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -52,11 +52,12 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 	}
 	
 	//地圖
-	if(stristr($inputStr, '地圖') != false) {
+	if(stristr($inputStr, '指路') != false) {
 		
 		$rplyArr = explode(' ',$inputStr);
 		if (count($rplyArr) == 1) {return buildTextMessage(''.$userName.'，你到底想讓我做啥?');}
 		require_once('./map.php');
+		return buildTextMessage(''.$maphop.'');
 	}
 	//推送
 	if(stristr($inputStr, '龘') != false||
