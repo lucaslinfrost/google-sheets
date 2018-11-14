bb447c74d3f464d82159bd44ea4f6fdcfc65d903
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-14 21:26:12 +0800

    Update item3.php

--- Boss/LINE/item3.php
+++ Boss/LINE/item3.php
@@ -1,7 +1,6 @@
 <?php
 //裝備頁(強化搜索介面)
 require_once('./LINEBotTiny.php');
-require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $googledataspi = getenv('googledataspi6');
@@ -12,8 +11,6 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             $json2 = file_get_contents($googledataspi);
             $data2 = json_decode($json2, true);
-            $c = new utf8_chinese;
-            $message['text'] = $c->gb2312_big5($message['text']);
             $code = explode('#', $message['text']);
             $alltext2 = "";
             foreach ($data2['feed']['entry'] as $item2) {
