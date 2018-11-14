4bf133cde622e754ffc6d1675ae8a6ebca4c1f54
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-14 21:22:47 +0800

    Update produce.php

--- Boss/LINE/produce.php
+++ Boss/LINE/produce.php
@@ -1,7 +1,6 @@
 <?php
 //生產頁(文字介面)
 require_once('./LINEBotTiny.php');
-require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $googledataspi2 = getenv('googledataspi2');
@@ -12,8 +11,6 @@ foreach ($client->parseEvents() as $event) {
             $message = $event['message'];
             $json = file_get_contents($googledataspi2);
             $data = json_decode($json, true);
-            $c = new utf8_chinese;
-            $message['text'] = $c->gb2312_big5($message['text']);
             $code = explode(' ', $message['text']);
             $alltext = "";
             $mline = "\n--------  分°Д°行  --------\n";
