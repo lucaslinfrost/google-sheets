89583cd412a2cbaee47badf2cc56420f2186fb03
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2018-11-18 21:06:12 +0800

    Update map.php

--- Boss/LINE/map.php
+++ Boss/LINE/map.php
@@ -691,27 +691,23 @@ $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $googledataspi = getenv('googledataspi9');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 foreach ($client->parseEvents() as $event) {
-switch ($event['type']) {       
-    case 'message':
-        $message = $event['message'];
-        $json = file_get_contents($googledataspi);
-        $data = json_decode($json, true);
-        $code = explode('#', $message['text']);       
-        foreach ($data['feed']['entry'] as $item) {
-            $keywords = explode(',', $item['gsx$key']['$t']);
-            foreach ($keywords as $keyword) {
-                if (strcmp($code[1], $keyword) === 0) {                      
-                $mappush1 = $item['gsx$mapn']['$t'];     
-                }else{       
-                $mappush1 = $code[1];      
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            $json = file_get_contents($googledataspi);
+            $data = json_decode($json, true);
+            $code = explode('#', $message['text']);
+            foreach ($data['feed']['entry'] as $item) {
+                $keywords = explode(',', $item['gsx$key']['$t']);
+                foreach ($keywords as $keyword) {
+                    if (strcmp($code[1], $keyword) === 0) {
+                    $code[1] = $item['gsx$mapn']['$t']; 
+                    }
+                    if (strcmp($code[2], $keyword) === 0) {
+                    $code[2] = $item['gsx$mapn']['$t']; 
+                    }
                 }
-                if (strcmp($code[2], $keyword) === 0) {                      
-                $mappush2 = $item['gsx$mapn']['$t'];     
-                }else{       
-                $mappush2 = $code[2];      
-                }
-}
-}
+            }
             break;
         default:
             error_log("Unsupporeted event type: " . $event['type']);
@@ -720,7 +716,7 @@ switch ($event['type']) {
 };
 
 $g = new Graph($graph);
-$g->leastHops($mappush1, $mappush2);
+$g->leastHops($code[1], $code[2]);
 
 class Graph
 {
