ea70a0a01af617840c7abe8eb22b357452f894db
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-04-26 07:15:23 +0800

    Update public.php

--- Boss/LINE/public.php
+++ Boss/LINE/public.php
@@ -30,7 +30,7 @@ switch ($event['type']) {
             foreach ($keywords as $keyword) {
                 if($code[1] === Null) {$code[1] = "公告";}
                 if (strcmp($code[1], $keyword) === 0) {
-                    $data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."\n\n(ノ・ω・)ノ發佈者\n".$item['gsx$name']['$t']."ヾ(・ω・ヾ)";
+                    $data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."\n\n(ノ・ω・)ノ發佈者\n".$item['gsx$name']['$t']."\nヾ(・ω・ヾ)發佈時間\n".$item['gsx$date']['$t'];
                }
             }
         }    
