6e3b6473a7d1e51fe3f4d00745e3a3139714cf9c
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-04-30 17:23:44 +0800

    Update public.php

--- Boss/LINE/public.php
+++ Boss/LINE/public.php
@@ -18,7 +18,7 @@ switch ($event['type']) {
 
         // 將Google表單轉成JSON資料
         $json = file_get_contents($googledataspi);
-        if ($json === "") {$json = file_get_contents('./data/public.json');}
+        if ($json === Null) {$json = file_get_contents('./data/public.json');}
         $data = json_decode($json, true); 
         $data999 = "";
         $code = explode(' ', $message['text']);
