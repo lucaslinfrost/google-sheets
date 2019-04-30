d10d954c596c315f27a2c59d202d9f9300fa11da
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-05-01 00:08:24 +0800

    Update weather.php

--- Boss/LINE/weather.php
+++ Boss/LINE/weather.php
@@ -26,9 +26,11 @@ switch ($event['type']) {
     curl_setopt($curlobj, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36'); // 伪造一个 HTTP_USER_AGENT 信息，解决为将对象引用设置到对象的实例问题
     $rtn = curl_exec($curlobj);   
     $rtn = str_replace("</string>","",$rtn);
-
+    $rtn = explode('<string>', $rtn);
+    $rtns = explode('；', $rtn[11]);
+    $content = "【".$rtn[1]." / ".$rtn[2]."】\n更新時間 : ".$rtn[5]."\n\n".$rtns[0]."\n".$rtns[1]."\n".$rtns[2]."\n".$rtns[3]."\n\n--------三日天氣--------\n".$rtn[7]."\n".$rtn[6]."\n".$rtn[8]."\n\n".$rtn[14]."\n".$rtn[13]."\n".$rtn[15]."\n\n".$rtn[19]."\n".$rtn[18]."\n".$rtn[20];
     if(!curl_errno($curlobj)) {
-    
+    $content = "沒有。";
     } else {
         $rtn = "沒有。";
     }  
