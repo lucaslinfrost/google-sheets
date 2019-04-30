bc5c105a7d44f4a2727457608ce30b7e188c4552
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-05-01 05:50:10 +0800

    Update public.php

--- Boss/LINE/public.php
+++ Boss/LINE/public.php
@@ -17,7 +17,7 @@ switch ($event['type']) {
         $message = $event['message'];
 
         // 將Google表單轉成JSON資料
-        $json = file_get_contents('./data/public.json');
+        $json = file_get_contents($googledataspi);
         $data = json_decode($json, true); 
         $data999 = "";
         $code = explode(' ', $message['text']);
