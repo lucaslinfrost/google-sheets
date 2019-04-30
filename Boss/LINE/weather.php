83ea80653c2f3277a78e536c00d94992b1d0ea97
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-05-01 00:55:43 +0800

    Update weather.php

--- Boss/LINE/weather.php
+++ Boss/LINE/weather.php
@@ -29,15 +29,19 @@ switch ($event['type']) {
     $rtn = str_replace("</string>","",$rtn);
     $rtn = str_replace(" ","",$rtn);
     $rtn = explode('<string>', $rtn);
+    if ($rtn[1] !== "查询结果为空！") {
+    $content = "沒有找到。(๑•́ ₃ •̀๑)";
+    }else{
     $rtns = explode('；', $rtn[11]);
     $rtns[0] = str_replace("实况：","实况：\n",$rtns[0]);
     $rtns[3] = str_replace("空气","\n空气",$rtns[3]);
     $content = "地區 : ".$rtn[1]."城市 : ".$rtn[2]."\n".$rtns[0]."\n".$rtns[1]."\n".$rtns[2]."\n".$rtns[3]."\n--------三日天氣--------\n".$rtn[7]."".$rtn[6]."".$rtn[8]."\n".$rtn[14]."".$rtn[13]."".$rtn[15]."\n".$rtn[19]."".$rtn[18]."".$rtn[20];
     $c = new utf8_chinese;
     $content = $c->gb2312_big5($content);
+    }
     if(!curl_errno($curlobj)) {
     } else {
-    $content = "沒有。";
+    $content = "沒有找到。(๑•́ ₃ •̀๑)";
     }  
          
         break;
