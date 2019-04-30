ddb8fd423d9e2bc363284e3f16271d52f1b9d48a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-05-01 00:41:51 +0800

    Update weather.php

--- Boss/LINE/weather.php
+++ Boss/LINE/weather.php
@@ -32,7 +32,7 @@ switch ($event['type']) {
     $rtn = explode('<string>', $rtn);
     $rtns = explode('；', $rtn[11]);
     $rtns[3] = str_replace("空气","\n空气",$rtns[3]);
-    $content = "更新時間 : ".$rtnt[0]."".$rtn[1]."".$rtn[2]."\n".$rtns[0]."\n".$rtns[1]."\n".$rtns[2]."\n".$rtns[3]."\n--------三日天氣--------\n".$rtn[7]."".$rtn[6]."".$rtn[8]."\n".$rtn[14]."".$rtn[13]."".$rtn[15]."\n".$rtn[19]."".$rtn[18]."".$rtn[20];
+    $content = "更新時間 : ".$rtnt[1]."\n".$rtn[1]."".$rtn[2]."\n".$rtns[0]."\n".$rtns[1]."\n".$rtns[2]."\n".$rtns[3]."\n--------三日天氣--------\n".$rtn[7]."".$rtn[6]."".$rtn[8]."\n".$rtn[14]."".$rtn[13]."".$rtn[15]."\n".$rtn[19]."".$rtn[18]."".$rtn[20];
     $c = new utf8_chinese;
     $content = $c->gb2312_big5($content);
     if(!curl_errno($curlobj)) {
