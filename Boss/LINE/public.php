4ee187541c9152c4b8e875c9427eb1a3fa1763fd
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-04-30 16:30:57 +0800

    Update public.php

--- Boss/LINE/public.php
+++ Boss/LINE/public.php
@@ -17,8 +17,14 @@ switch ($event['type']) {
         $message = $event['message'];
 
         // 將Google表單轉成JSON資料
-        ini_set('user_agent', 'OreOreAgent');
-        $json = file_get_contents($googledataspi);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch,CURLOPT_URL,$googledataspi);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.A.B.C Safari/525.13");
+        $json = curl_exec($ch);
+        curl_close($ch);
+        
         $data = json_decode($json, true); 
         $data999 = "";
         $code = explode(' ', $message['text']);
