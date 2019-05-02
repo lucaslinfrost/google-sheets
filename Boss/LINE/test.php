3b748dd3b2ef0fae93eff5e96616054e7c55111a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-05-03 01:42:45 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -29,7 +29,7 @@ switch ($event['type']) {
         $data0 = $xPath->evaluate('string(//*[@id="newsList"]/li[1]/a/text())'); //第一筆文字
         $data1 = $xPath->evaluate('string(//*[@id="newsList"]/li[1]/a/time/@datetime)'); //第一筆日期
         $data2 = $xPath->evaluate('string(//*[@id="newsList"]/li[1]/a/@href)'); //第一筆網址
-        $candidate = array(
+        $dataall = array(
                             'thumbnailImageUrl' => 'https://imgur.com/KQsuipD.png',
                             'title' => $data0,
                             'text' => $data1,
@@ -40,7 +40,6 @@ switch ($event['type']) {
                                     )
                                 ),
                             );
-        array_push($dataall, $candidate);
         break;
     default:
         error_log("Unsupporeted event type: " . $event['type']);
