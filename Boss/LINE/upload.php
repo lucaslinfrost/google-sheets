bdfdc72c0e456e3bae691e5cd1b951828b8f1fde
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-05-03 06:26:13 +0800

    Add files via upload

---
+++ Boss/LINE/upload.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8" />
+      <title>Upload Hình Ảnh</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+   <body class="blurBg-false" style="background-color:#EBEBEB">
+      <!-- Start Formoid form-->
+      <link rel="stylesheet" href="formoid_files/formoid1/formoid-flat-red.css" type="text/css" />
+      <script type="text/javascript" src="formoid_files/formoid1/jquery.min.js"></script>
+      <div enctype="multipart/form-data" class="formoid-flat-red" style="background-color:#FFFFFF;font-size:14px;font-family:'Lato', sans-serif;color:#666666;max-width:480px;min-width:150px" method="post">
+         <div class="title">
+            <h2>Upload Images</h2>
+         </div>
+<?php
+$puaruvn = basename($_FILES["fileToUpload"]["name"]);
+$puarupham = strtolower(pathinfo($puaruvn,PATHINFO_EXTENSION));
+if($puarupham != "jpg" && $puarupham != "png" && $puarupham != "jpeg"
+&& $puarupham != "gif" ) {
+	echo '<div class="element-input"><label class="title">Kết Quả</label><input class="large" type="text" name="input" value="Sorry, only JPG, JPEG, PNG & GIF files are allowed." /></div>';
+}
+else
+{
+	$img = upload($_FILES["fileToUpload"]["tmp_name"]);
+echo '<div class="element-input"><label class="title">Kết Quả</label><input class="large" type="text" name="input" value="'.$img.'" /></div><div class="element-input"><label class="title">BBCode</label><input class="large" type="text" name="input" value="[img]'.$img.'[/img]" /></div><div class="element-input"><label class="title">Hình Ảnh</label><img src="'.$img.'" width="100%"></div>';
+}
+?>
+<div class="submit"><a href="/"><input type="submit" value="Quay Lại"/></a></div>
+      </div>
+   </body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+
+
+function upload($anh)
+{
+$client_id = '33a2f8feed009b3';
+$file = file_get_contents($anh);
+$url = 'https://api.imgur.com/3/image.json';
+$headers = array("Authorization: Client-ID $client_id");
+$pvars  = array('image' => base64_encode($file));
+$curl = curl_init();
+curl_setopt_array($curl, array(
+   CURLOPT_URL=> $url,
+   CURLOPT_TIMEOUT => 30,
+   CURLOPT_POST => 1,
+   CURLOPT_RETURNTRANSFER => 1,
+   CURLOPT_HTTPHEADER => $headers,
+   CURLOPT_POSTFIELDS => $pvars
+));
+$json_returned = curl_exec($curl); // blank response
+$img = json_decode($json_returned,true);
+return $img['data']['link'];
+curl_close ($curl); 
+}
+?>
