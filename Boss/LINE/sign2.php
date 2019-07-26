8b595d7eee1cd5b3ed9ff660410cff09801f55bc
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-07-26 20:54:15 +0800

    Update sign2.php

--- Boss/LINE/sign2.php
+++ Boss/LINE/sign2.php
@@ -43,15 +43,7 @@ switch ($event['type']) {
         // create domxpath instance
         $xPath = new DOMXPath($dom);
         // get all elements with a particular id and then loop through and print the href attribute
-        $data0 = $xPath->evaluate('string(//*[@id="newsList"]/li[1]/a/text())'); //第一筆文字
-        $data1 = $xPath->evaluate('string(//*[@id="newsList"]/li[1]/a/time/@datetime)'); //第一筆日期
-        $data2 = $xPath->evaluate('string(//*[@id="newsList"]/li[1]/a/@href)'); //第一筆網址
-        $data3 = $xPath->evaluate('string(//*[@id="newsList"]/li[2]/a/text())'); //第二筆文字
-        $data4 = $xPath->evaluate('string(//*[@id="newsList"]/li[2]/a/time/@datetime)'); //第二筆日期
-        $data5 = $xPath->evaluate('string(//*[@id="newsList"]/li[2]/a/@href)'); //第二筆網址
-        $data6 = $xPath->evaluate('string(//*[@id="newsList"]/li[3]/a/text())'); //第三筆文字
-        $data7 = $xPath->evaluate('string(//*[@id="newsList"]/li[3]/a/time/@datetime)'); //第三筆日期
-        $data8 = $xPath->evaluate('string(//*[@id="newsList"]/li[3]/a/@href)'); //第三筆網址
+        $datall = $xPath->evaluate('string(//*[@class="TODAY_CONTENT"]/text())'); //星座資訊
         
         break;
     default:
