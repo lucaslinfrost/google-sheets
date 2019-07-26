4636379b77d3978c6a4faec93d0fa3cf3716fe85
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-07-26 22:17:02 +0800

    Update sign2.php

--- Boss/LINE/sign2.php
+++ Boss/LINE/sign2.php
@@ -16,18 +16,18 @@ switch ($event['type']) {
         $message['text'] = $c->gb2312_big5($message['text']);
         $code = explode(' ', $message['text']);
         
-        if (strpos($code[1], '白羊') !== false||strpos($code[1], '牡羊') !== false||strpos($code[1], '牧羊') !== false){$snum = '0';}
-        if (strpos($code[1], '金牛') !== false||strpos($code[1], '牡牛') !== false||strpos($code[1], '牧牛') !== false){$snum = '1';}
-        if (strpos($code[1], '雙子') !== false){$snum = '2';}
-        if (strpos($code[1], '巨蟹') !== false){$snum = '3';}
-        if (strpos($code[1], '獅子') !== false){$snum = '4';}
-        if (strpos($code[1], '處女') !== false){$snum = '5';}
-        if (strpos($code[1], '天秤') !== false||strpos($code[1], '天平') !== false){$snum = '6';}
-        if (strpos($code[1], '天蠍') !== false){$snum = '7';}
-        if (strpos($code[1], '射手') !== false){$snum = '8';}
-        if (strpos($code[1], '魔羯') !== false||strpos($code[1], '山羊') !== false||strpos($code[1], '摩羯') !== false){$snum = '9';}
-        if (strpos($code[1], '水瓶') !== false||strpos($code[1], '寶瓶') !== false){$snum = '10';}
-        if (strpos($code[1], '雙魚') !== false){$snum = '11';}
+        if (strpos($code[1], '白羊') !== false||strpos($code[1], '牡羊') !== false||strpos($code[1], '牧羊') !== false){$snum = '0';}else{
+        if (strpos($code[1], '金牛') !== false||strpos($code[1], '牡牛') !== false||strpos($code[1], '牧牛') !== false){$snum = '1';}else{
+        if (strpos($code[1], '雙子') !== false){$snum = '2';}else{
+        if (strpos($code[1], '巨蟹') !== false){$snum = '3';}else{
+        if (strpos($code[1], '獅子') !== false){$snum = '4';}else{
+        if (strpos($code[1], '處女') !== false){$snum = '5';}else{
+        if (strpos($code[1], '天秤') !== false||strpos($code[1], '天平') !== false){$snum = '6';}else{
+        if (strpos($code[1], '天蠍') !== false){$snum = '7';}else{
+        if (strpos($code[1], '射手') !== false){$snum = '8';}else{
+        if (strpos($code[1], '魔羯') !== false||strpos($code[1], '山羊') !== false||strpos($code[1], '摩羯') !== false){$snum = '9';}else{
+        if (strpos($code[1], '水瓶') !== false||strpos($code[1], '寶瓶') !== false){$snum = '10';}else{
+        if (strpos($code[1], '雙魚') !== false){$snum = '11';}else{$snum = '12';}
         
         $url = 'http://astro.click108.com.tw/daily_0.php?iAstro='.$snum;
         $ch = curl_init();
@@ -59,9 +59,12 @@ switch ($event['type']) {
         $data12 = $xPath->evaluate('string(//*[@class="LUCKY"][3]/h4/text())');
         $data13 = $xPath->evaluate('string(//*[@class="LUCKY"][4]/h4/text())');
         $data14 = $xPath->evaluate('string(//*[@class="LUCKY"][5]/h4/text())');
-
+            
+if ($snum === "12"){
+$dataall = "沒有這個星座。";
+}else{
 $dataall = $data0." (".$datatime.")\n\n".$data1."\n".$data2."\n\n".$data3."\n".$data4."\n\n".$data5."\n".$data6."\n\n".$data7."\n".$data8."\n\n今日短評：\n".$data9."\n\n幸運數字：".$data10."\n\n幸運顏色：".$data11."\n\n開運方位：".$data12."\n\n今日吉時：".$data13."\n\n幸運星座：".$data14;
-
+}
         break;
     default:
         error_log("Unsupporeted event type: " . $event['type']);
