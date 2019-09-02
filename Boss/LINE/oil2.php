335c0221fc4a5f54e193b4a6fc918982d0223fd3
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-09-03 01:07:48 +0800

    Update oil2.php

--- Boss/LINE/oil2.php
+++ Boss/LINE/oil2.php
@@ -28,6 +28,7 @@ switch ($event['type']) {
         $data00 = $xPath->evaluate('string(//*[@id="gas-price"]/ul/li[3]/p/text()[1])');
         $data0 = $xPath->evaluate('string(//*[@id="gas-price"]/ul/li[3]/p/text()[2])');
         $data1 = $xPath->evaluate('string(//*[@id="gas-price"]/ul/li[3]/h2/text())');
+        $data01 = $xPath->evaluate('string(//*[@id="gas-price"]/ul/li[3]/h2/em/text())');
         $data2 = $xPath->evaluate('string(//*[@id="cpc"]/ul/li[1]/text())');
         $data3 = $xPath->evaluate('string(//*[@id="cpc"]/ul/li[2]/text())');
         $data4 = $xPath->evaluate('string(//*[@id="cpc"]/ul/li[3]/text())');
@@ -39,7 +40,7 @@ switch ($event['type']) {
         $data10 = $xPath->evaluate('string(//*[@id="gas-price"]/ul/li[1]/text())');
         $datatime = $xPath->evaluate('string(//*[@id="main"]/p/time/text())');
             
-$dataall = "最後更新時間\n".$datatime."\n\n".$data00."".$data0."".$data1."\n\n今日中油油價\n92 : ".$data2."\n95 : ".$data3."\n98 : ".$data4."\n柴油 : ".$data5."\n\n今日台塑油價\n92 : ".$data6."\n95 : ".$data7."\n98 : ".$data8."\n柴油 : ".$data9."\n\n柴油預計調整\n".$data10;
+$dataall = "最後更新時間\n".$datatime."\n\n".$data00."".$data0."".$data1."".$data01."\n\n今日中油油價\n92 : ".$data2."\n95 : ".$data3."\n98 : ".$data4."\n柴油 : ".$data5."\n\n今日台塑油價\n92 : ".$data6."\n95 : ".$data7."\n98 : ".$data8."\n柴油 : ".$data9."\n\n柴油預計調整\n".$data10;
 
         break;
     default:
