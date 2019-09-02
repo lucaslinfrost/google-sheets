f4147c55ab9726cd86db7f7b9f3cff5bf730d26a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2019-09-03 00:20:48 +0800

    Update oil2.php

--- Boss/LINE/oil2.php
+++ Boss/LINE/oil2.php
@@ -1,7 +1,6 @@
 <?php
 //油價功能介面 (文字版)
 require_once('./LINEBotTiny.php');
-require_once('./utf8_chinese.class.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
@@ -11,24 +10,8 @@ switch ($event['type']) {
     case 'message':
         // 讀入訊息
         $message = $event['message'];
-        $c = new utf8_chinese;
-        $message['text'] = $c->gb2312_big5($message['text']);
         $code = explode(' ', $message['text']);
-        $snum = '999';
-        if (strpos($code[1], '白羊') !== false||strpos($code[1], '牡羊') !== false||strpos($code[1], '牧羊') !== false||strpos($code[1], '♈') !== false){$snum = '0';}
-        if (strpos($code[1], '金牛') !== false||strpos($code[1], '牡牛') !== false||strpos($code[1], '牧牛') !== false||strpos($code[1], '♉') !== false){$snum = '1';}
-        if (strpos($code[1], '雙子') !== false||strpos($code[1], '♊') !== false){$snum = '2';}
-        if (strpos($code[1], '巨蟹') !== false||strpos($code[1], '♋') !== false){$snum = '3';}
-        if (strpos($code[1], '獅子') !== false||strpos($code[1], '♌') !== false){$snum = '4';}
-        if (strpos($code[1], '處女') !== false||strpos($code[1], '♍') !== false){$snum = '5';}
-        if (strpos($code[1], '天秤') !== false||strpos($code[1], '天平') !== false||strpos($code[1], '♎') !== false){$snum = '6';}
-        if (strpos($code[1], '天蠍') !== false||strpos($code[1], '♏') !== false){$snum = '7';}
-        if (strpos($code[1], '射手') !== false||strpos($code[1], '♐') !== false){$snum = '8';}
-        if (strpos($code[1], '魔羯') !== false||strpos($code[1], '山羊') !== false||strpos($code[1], '摩羯') !== false||strpos($code[1], '♑') !== false){$snum = '9';}
-        if (strpos($code[1], '水瓶') !== false||strpos($code[1], '寶瓶') !== false||strpos($code[1], '♒') !== false){$snum = '10';}
-        if (strpos($code[1], '雙魚') !== false||strpos($code[1], '♓') !== false){$snum = '11';}
-        
-        $url = 'http://astro.click108.com.tw/daily_0.php?iAstro='.$snum;
+        $url = 'https://gas.goodlife.tw/';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_FILE, fopen('php://stdout', 'w'));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
@@ -59,11 +42,9 @@ switch ($event['type']) {
         $data13 = $xPath->evaluate('string(//*[@class="LUCKY"][4]/h4/text())');
         $data14 = $xPath->evaluate('string(//*[@class="LUCKY"][5]/h4/text())');
             
-if ($snum === "999"){
-$dataall = "沒有這個星座的運勢資訊。";
-}else{
+
 $dataall = $data0." (".$datatime.")\n\n".$data1."\n".$data2."\n\n".$data3."\n".$data4."\n\n".$data5."\n".$data6."\n\n".$data7."\n".$data8."\n\n今日短評：\n".$data9."\n\n幸運數字：".$data10."\n\n幸運顏色：".$data11."\n\n開運方位：".$data12."\n\n今日吉時：".$data13."\n\n幸運星座：".$data14;
-}
+
         break;
     default:
         error_log("Unsupporeted event type: " . $event['type']);
