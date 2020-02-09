aaeed3ee1c92fb95bb8719e90b042d00eb5edc1b
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-02-09 08:56:28 +0800

    Update skill.php

--- Boss/LINE/skill.php
+++ Boss/LINE/skill.php
@@ -31,7 +31,7 @@ foreach ($client->parseEvents() as $event) {
                                     'label' => $item['gsx$skname']['$t']."".$item['gsx$sklv']['$t'],
                                     'text' => "測試",
                                     );
-				
+				array_push($per3skill, $allskill);
 				}
 				
 			
@@ -39,7 +39,7 @@ foreach ($client->parseEvents() as $event) {
                             'thumbnailImageUrl' => 'https://imgur.com/KQsuipD.png',
                             'title' => $item['gsx$job']['$t'],
                             'text' => $item['gsx$job']['$t'],
-                            'actions' => $allskill,
+                            'actions' => $per3skill,
                             );
                         array_push($result, $candidate);
 		   }
