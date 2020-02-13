155ce8b1a55d0ac49e84997ef8f155ccaa2896ca
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-02-13 18:37:01 +0800

    Update sign2.php

--- Boss/LINE/sign2.php
+++ Boss/LINE/sign2.php
@@ -39,8 +39,7 @@ switch ($event['type']) {
         // create document object model
         $dom = new DOMDocument();
         // load html into document object model
-        //@$dom->loadHTML('<?xml encoding="UTF-8">' . $html);
-        @$dom->loadHTML(mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8'));
+        @$dom->loadHTML('<?xml encoding="UTF-8">' . $html);
         // create domxpath instance
         $xPath = new DOMXPath($dom);
         // get all elements with a particular id and then loop through and print the href attribute
