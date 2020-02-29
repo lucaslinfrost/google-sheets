1effbe98ec75914484a8f71e3b81e40d938bece5
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-02-29 13:24:41 +0800

    Update equipformula.php

--- Boss/LINE/equipformula.php
+++ Boss/LINE/equipformula.php
@@ -8,40 +8,49 @@ foreach ($client->parseEvents() as $event) {
     switch ($event['type']) {
         case 'message':
             $message = $event['message'];
-            $code = explode(' ', $message['text']);
+            $code = explode('#', $message['text']);
             $alltext = "";
             $a = "";
             $b = "";
-            $countno = 0;
+            $mline = "\n--------  分°Д°行  --------\n";
 
 $json0 = file_get_contents('./data/equip.json');
 $data0 = json_decode($json0, true);
+$json1 = file_get_contents('./data/autoformula.json');
+$data1 = json_decode($json1, true);
+            
 foreach ($data0['feed']['entry'] as $item) {
 $keywords = explode(',', $item['gsx$key']['$t']);
 foreach ($keywords as $keyword) {
 if (strcmp($code[1], $keyword) === 0) {
 $a = "公式 :
 ".$item['gsx$equipformula']['$t'];
-unset($json0, $data0, $keywords, $keyword);
-}
-}
-}
 
-
-$json1 = file_get_contents('./data/autoformula.json');
-$data1 = json_decode($json1, true);
-foreach ($data1'feed']['entry'] as $item) {
-$keywords = $item['gsx$autonum']['$t'];
-foreach ($keywords as $keyword) {
-if (strpos($a, $keyword) !== false) {
+foreach ($data1'feed']['entry'] as $item1) {
+$keywords1 = $item1['gsx$autonum']['$t'];
+foreach ($keywords1 as $keyword1) {
+if (strpos($a, $keyword1) !== false) {
 $b = "
 
 自動技能"."【".$item['gsx$autoname']['$t']."】
 ".$item['gsx$autoformula']['$t'];
-unset($json1, $data1, $keywords, $keyword);
+$alltext = $alltext."".$a."".$b;
+$alltext = $alltext."".$mline;
+}else{
+$alltext = $alltext."".$a;
+$alltext = $alltext."".$mline;
 }
 }
 }
+
+
+}
+}
+}
+
+
+
+
 
 
 
