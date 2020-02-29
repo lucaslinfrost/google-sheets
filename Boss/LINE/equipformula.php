414a5b428cb2347cb731df347deaee040caf3f47
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-02-29 13:14:14 +0800

    Update equipformula.php

--- Boss/LINE/equipformula.php
+++ Boss/LINE/equipformula.php
@@ -14,7 +14,6 @@ foreach ($client->parseEvents() as $event) {
             $b = "";
             $countno = 0;
 
-if ($countno === 0) {
 $json0 = file_get_contents('./data/equip.json');
 $data0 = json_decode($json0, true);
 foreach ($data0['feed']['entry'] as $item) {
@@ -27,25 +26,23 @@ unset($json0, $data0, $keywords, $keyword);
 }
 }
 }
-$countno = $countno+1;   
-}
 
 
-if ($countno === 1 {
 $json1 = file_get_contents('./data/autoformula.json');
 $data1 = json_decode($json1, true);
 foreach ($data1'feed']['entry'] as $item) {
 $keywords = $item['gsx$autonum']['$t'];
 foreach ($keywords as $keyword) {
-if (strpos($code[1], $keyword) !== false) {
-$a = "公式 :
-".$item['gsx$equipformula']['$t'];
-unset($json0, $data0, $keywords, $keyword);
+if (strpos($a, $keyword) !== false) {
+$b = "
+
+自動技能"."【".$item['gsx$autoname']['$t']."】
+".$item['gsx$autoformula']['$t'];
+unset($json1, $data1, $keywords, $keyword);
 }
 }
 }
-$countno = $countno+1;   
-}
+
 
 
             break;
