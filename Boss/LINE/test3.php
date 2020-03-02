dc7584f8bc1e85392884ab47f1d67d55b2ea9005
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-03 06:24:02 +0800

    Update test3.php

--- Boss/LINE/test3.php
+++ Boss/LINE/test3.php
@@ -1,9 +1,10 @@
 <?php
-$file = fopen("./exampleJson/test.json", "a+");
-$data = array();
-$data['a'] = 'test';
-$data['b'] = 'bbb';
-// 把PHP数组转成JSON字符串
-$json_obj = json_decode($data);
-// 写入文件
-fwrite($file, $json_obj); 
+require_once('./LINEBotTiny.php');
+$access_token = getenv('LINE_CHANNEL_ACCESSTOKEN');
+$secret = getenv('LINE_CHANNEL_SECRET');
+$bot = new LINEBotTiny($access_token, $secret);
+$newjson = file_get_contents('https://spreadsheets.google.com/feeds/list/1DF1BBZUPVGWHLN6V4W_2G2ZzfIo3iU67Mr1Fu85ZMMg/od6/public/values?alt=json');
+$newdata = json_decode($newjson, true);
+
+$file = fopen("./data/m&d.json", "w+");
+fwrite($file, $newdata);
