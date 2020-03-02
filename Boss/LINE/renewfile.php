161a1aa9bfea907cb37b92e1d53be4f3cf200a83
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-03 07:32:39 +0800

    Update renewfile.php

--- Boss/LINE/renewfile.php
+++ Boss/LINE/renewfile.php
@@ -9,9 +9,18 @@ switch ($event['type']) {
 case 'message':
 $message = $event['message'];
 $code = explode('#', $message['text']);
+$countno = 0;
     
 if (strcmp($code[1], "全部") === 0) {
-$updatelog = "全部資料已更新。\n(功能尚未實裝)";
+if ($countno === 0) {$getfileaddress = getenv('monster_item'); $update = file_get_contents($getfileaddress);$file = fopen("./data/m&d.json", "w+");fwrite($file, $update);$countno = $countno+1;}
+if ($countno === 1) {$getfileaddress = getenv('production');$update = file_get_contents($getfileaddress);$file = fopen("./data/production.json", "w+");fwrite($file, $update);$countno = $countno+1;}
+if ($countno === 2) {$getfileaddress = getenv('equip');$update = file_get_contents($getfileaddress);$file = fopen("./data/equip.json", "w+");fwrite($file, $update);$countno = $countno+1;}	
+if ($countno === 3) {$getfileaddress = getenv('rock');$update = file_get_contents($getfileaddress);$file = fopen("./data/rock.json", "w+");fwrite($file, $update);$countno = $countno+1;}	
+if ($countno === 4) {$getfileaddress = getenv('star');$update = file_get_contents($getfileaddress);$file = fopen("./data/star.json", "w+");fwrite($file, $update);$countno = $countno+1;}	
+if ($countno === 5) {$getfileaddress = getenv('map');$update = file_get_contents($getfileaddress);$file = fopen("./data/map.json", "w+");fwrite($file, $update);$countno = $countno+1;}	
+if ($countno === 6) {$getfileaddress = getenv('seed');$update = file_get_contents($getfileaddress);$file = fopen("./data/seed.json", "w+");fwrite($file, $update);$countno = $countno+1;}	
+if ($countno === 7) {$getfileaddress = getenv('drill');$update = file_get_contents($getfileaddress);$file = fopen("./data/drill.json", "w+");fwrite($file, $update);$countno = $countno+1;}	
+$updatelog = "全部資料已更新。";
 }
 		
 if (strcmp($code[1], "怪物") === 0) {
