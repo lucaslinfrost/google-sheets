1a58f5bec18448d1638a03d8b57b535efc649a2b
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-03 04:24:38 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -8,16 +8,6 @@ $json_string = file_get_contents('php://input');
 $file = fopen("./exampleJson/test.json", "a+");
 $json_obj = json_decode($json_string);
 
-  if (empty($json_obj->{"birthdays"})) {
-    $new_id = 1;
-  }
-  else {
-    $used_nums = [];
-    foreach ($json_obj->{"birthdays"} as $user) {
-      array_push($used_nums, $user->{"birthdayId"});
-    }
-    $new_id = max($used_nums) + 1;
-  }
 
 //***將收到的資料存到文字黨做紀錄***
 fwrite($file, "第[".$new_id."]筆資料。\n"); 
@@ -34,6 +24,6 @@ $message_data = doType($type,$message->{"text"});
 $post_data = doPostData($reply_token,$message_data);
 //***將post_data(BOT發送訊息)存到文字黨做紀錄***
 fwrite($file, "系統回復資訊\n");
-fwrite($file, json_encode($post_data)."\n\n");
+fwrite($file, json_encode($post_data)."\n");
 //***將訊息發出去然後關閉寫入***
 doBotPost($post_data,$access_token,$file);
