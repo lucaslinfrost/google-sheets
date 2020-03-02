dfa75d316a6c45ac50e17b7be9754ff3703183f8
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-02 13:37:11 +0800

    Update authorization.php

--- Boss/LINE/authorization.php
+++ Boss/LINE/authorization.php
@@ -1,11 +1,12 @@
 <?php
+require_once('./LINEBotTiny.php');
 $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $owner = getenv('Owner');
 $bot = new LINEBotTiny($channelAccessToken, $channelSecret);
-foreach ($client->parseEvents() as $event) {
-switch ($event['type']) {
-case 'message':
+foreach ($bot->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
 $source = $event['source'];
 $userId = $source['userId'];
 $roomId = $source['roomId'];
