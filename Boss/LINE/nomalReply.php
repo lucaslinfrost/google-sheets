8cc891c3b862bece9e7e4a07a78f10a95e24c514
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-03 12:18:10 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -526,7 +526,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 	}
 	
   //沒有觸發關鍵字則是這個
-	require_once('./star.php');
+	require_once('./replyfromlearn.php');
 	if ($talkreply === "") {
 	$rplyArr = $content[0]['text'];
 	return buildTextMessage($rplyArr[Dice(count($rplyArr))-1]);
