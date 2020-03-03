1e4f39a71f916fda486372f8ba40d794fd813242
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-04 06:43:06 +0800

    Update learn.php

--- Boss/LINE/learn.php
+++ Boss/LINE/learn.php
@@ -31,11 +31,8 @@ $talkreply = "不能輸入空值。\n格式 :\n老大學#關鍵字#回答句\n�
             $replyfromlearn = $code[2];
             $json = file_get_contents('./exampleJson/textReply.json');
             $upfile = json_decode($json, true);
-            $content = json_decode($json, true);
-            $file = fopen("./exampleJson/textReply.json", "w+");
-            array_push($upfile, $update);
             $upfile = json_encode($upfile, JSON_UNESCAPED_UNICODE);
-            foreach($content as $txtChack){
+            foreach($upfile as $txtChack){
             foreach($txtChack['chack'] as $chack){
             if(stristr($code[1], $chack) != false){
             $talkreply = "這個我已經學過了喔。";
@@ -43,13 +40,16 @@ $talkreply = "不能輸入空值。\n格式 :\n老大學#關鍵字#回答句\n�
                $update = array ('chack' => array ($learnword),'text' => array ($replyfromlearn),);
                if (strpos($learnword, ";") !== false) {$learnword = explode(";", $code[1]);$update = array ('chack' => $learnword,'text' => $replyfromlearn,);$code[1] = str_replace(";", "或", $code[1]);}
                if (strpos($replyfromlearn, ";") !== false) {$replyfromlearn = explode(";", $code[2]);$update = array ('chack' => $learnword,'text' => $replyfromlearn,);$code[2] = str_replace(";", "或", $code[2]);}
+               $file = fopen("./exampleJson/textReply.json", "w+");
+               array_push($upfile, $update);
                fwrite($file, $upfile);
                $talkreply = "我已經學會了看到[".$code[1]."]\n就要回答[".$code[2]."]。";
+               fclose($file);
             }
             break;
             break;
             }}
-           fclose($file);
+           //fclose($file);
          }
       }
    }
