0493d39d8b551f4054034367f8203c1ff5dd07c8
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-04 06:50:52 +0800

    Update learn.php

--- Boss/LINE/learn.php
+++ Boss/LINE/learn.php
@@ -35,6 +35,7 @@ $talkreply = "不能輸入空值。\n格式 :\n老大學#關鍵字#回答句\n�
             foreach($txtChack['chack'] as $chack){
             if(stristr($code[1], $chack) != false){
             $talkreply = "這個我已經學過了喔。";
+            break;break;
             }else{
                $update = array ('chack' => array ($learnword),'text' => array ($replyfromlearn),);
                if (strpos($learnword, ";") !== false) {$learnword = explode(";", $code[1]);$update = array ('chack' => $learnword,'text' => $replyfromlearn,);$code[1] = str_replace(";", "或", $code[1]);}
@@ -48,10 +49,8 @@ $talkreply = "不能輸入空值。\n格式 :\n老大學#關鍵字#回答句\n�
                fwrite($file, $upfile);
                $talkreply = "我已經學會了看到[".$code[1]."]\n就要回答[".$code[2]."]。";
                fclose($file);
-            }
-            break;
-            break;
-            }}
+               break;break;
+            }}}
            //fclose($file);
          }
       }
