5d4982c9d4d2be5d4b10b59c04e471762e94fc63
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-04 11:15:01 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -5,23 +5,24 @@ $channelAccessToken = getenv('LINE_CHANNEL_ACCESSTOKEN');
 $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
-        //get all your data on file
-        $json = file_get_contents('./exampleJson/textReply.json');
-        // decode json to associative array
-        $data = json_decode($json, true);
-        // get array index to delete
-        $arr_index = array();
+$data = file_get_contents('./exampleJson/textReply.json');
+$json_arr = json_decode($data, true);
+// get array index to delete
+$arr_index = array();
+foreach ($json_arr as $key => $value) {
+$n = array ("11");
+if ($value['chack'] == $n) {
+$arr_index[] = $key;
+}
+}
 
-        foreach ($data as $value) {
-         foreach($value['chack'] as $key => $chack){       
-                
-        //if ($value == "11") {
-        if(strcmp("11", $chack) === 0){
-        unset($data[$key]);
-        break;break;}}}
+// delete data
+foreach ($arr_index as $i) {
+unset($json_arr[$i]);
+}
 
-        // rebase array
-        $data = array_values($data);
-        // encode array to json and save to file
-        file_put_contents('./exampleJson/textReply.json', json_encode($data, JSON_UNESCAPED_UNICODE));
-  
+// rebase array
+$json_arr = array_values($json_arr);
+
+// encode array to json and save to file
+file_put_contents('./exampleJson/textReply.json', json_encode($json_arr, JSON_UNESCAPED_UNICODE));
