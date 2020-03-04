0cbfcd178849ff7b3483f77351bc4b50e73ab901
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-04 10:26:16 +0800

    Update test.php

--- Boss/LINE/test.php
+++ Boss/LINE/test.php
@@ -10,39 +10,29 @@ foreach ($client->parseEvents() as $event) {
 switch ($event['type']) {       
     case 'message':
         $message = $event['message'];
-        $code = explode(' ', $message['text']);
-        // 資料起始從feed.entry          
-        
+        $code = explode(' ', $message['text']);          
         
         //get all your data on file
-        $json = file_get_contents('teste_data.json');
+        $json = file_get_contents('./exampleJson/textReply.json');
         // decode json to associative array
         $data = json_decode($json, true);
-
         // get array index to delete
         $arr_index = array();
         foreach ($data as $key => $value) {
-        if ($value['YOUR KEY'] == SOME VALUE TO COMPARE) {
+        if ($value['chack'] == "11") {
         $arr_index[] = $key;
         }
+        break;
         }
-
         // delete data
         foreach ($arr_index as $i) {
         unset($data[$i]);
+        break;
         }
-
         // rebase array
         $data = array_values($data);
-
         // encode array to json and save to file
-        file_put_contents('teste_data.json', json_encode($data));
-        
-        
-        
-        
-        
-        
+        file_put_contents('./exampleJson/textReply.json', json_encode($data));
         
         break;
     default:
