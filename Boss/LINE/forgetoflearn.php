434ccf916b106a00f79f54ef6b5d902d11b793f3
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-04 11:24:15 +0800

    Update and rename test.php to forgetoflearn.php

---
+++ Boss/LINE/forgetoflearn.php
@@ -0,0 +1,42 @@
+<?php
+//忘記學習過的字
+require_once('./LINEBotTiny.php');
+$access_token = getenv('LINE_CHANNEL_ACCESSTOKEN');
+$secret = getenv('LINE_CHANNEL_SECRET');
+$bot = new LINEBotTiny($access_token, $secret);
+$message = $event['message'];
+$value = trim($message['text']);
+$value = preg_replace("/\s(?=)/", "", $value);
+$code = explode("#", $value);
+
+foreach ($bot->parseEvents() as $event) {
+switch ($event['type']) {
+case 'message':
+
+$data = file_get_contents('./exampleJson/textReply.json');
+$json_arr = json_decode($data, true);
+// get array index to delete
+$arr_index = array();
+foreach ($json_arr as $key => $value) {
+$n = array ("\"".$code[1]."\"");
+if ($value['chack'] == $n) {
+$arr_index[] = $key;
+break;
+}
+}
+// delete data
+foreach ($arr_index as $i) {
+unset($json_arr[$i]);
+break;
+}
+// rebase array
+$json_arr = array_values($json_arr);
+// encode array to json and save to file
+file_put_contents('./exampleJson/textReply.json', json_encode($json_arr, JSON_UNESCAPED_UNICODE));
+    
+   break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
