b6ac976ddf7bf684ebe8178a7d8b966d410bf0e3
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-03-05 23:36:13 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -77,6 +77,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 			return $messages->send($replyArr);
 		}else{
 		$productionspare = substr($productionspare, 0, -1);
+		$productionspare = $productionspare." ";
 		$messages = new MutiMessage();
 			$replyArr = Array(
 				$messages->text($alltext1),
