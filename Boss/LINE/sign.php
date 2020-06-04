b55ad4c0104b5e2bac3acf05be0e160428f7b737
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-06-04 21:26:56 +0800

    Update sign.php

--- Boss/LINE/sign.php
+++ Boss/LINE/sign.php
@@ -26,10 +26,8 @@ switch ($event['type']) {
             $keywords = explode(',', $item['gsx$key']['$t']);
             // 以關鍵字比對文字內容
             foreach ($keywords as $keyword) {
-                if (strpos($code[1], $keyword) === 0) {
+                if (strpos($code[1], $keyword) !== false) {
                 $dataall = $item['gsx$data1']['$t']." (".$datatime.")\n\n".$item['gsx$data2']['$t']."\n".$item['gsx$data3']['$t']."\n\n".$item['gsx$data4']['$t']."\n".$item['gsx$data5']['$t']."\n\n".$item['gsx$data6']['$t']."\n".$item['gsx$data7']['$t']."\n\n".$item['gsx$data8']['$t']."\n".$item['gsx$data9']['$t'];
-                }else{
-                $dataall = "沒有這個星座的運勢資訊。";
                 }
             }
         }       
