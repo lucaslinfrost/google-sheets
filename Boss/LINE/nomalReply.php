e7bdd45161e4d7011bae235e2197ca46fa94dc24
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-07-06 16:57:26 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -167,8 +167,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 	return buildTextMessage(''.$maphop.'');
 	}
 	//基本查詢
-	if(stristr($inputStr, '查') != false) {
-	$inputStr = preg_replace('/\xa3([\xa1-\xfe])/e', 'chr(ord(\1)-0x80)', $inputStr);  
+	if(stristr($inputStr, '查') != false) { 
 	$rplyArr = explode('#',$inputStr);
 	if (count($rplyArr) == 1) {return buildTextMessage(''.$userName.'，指令格式錯誤。');}
         return buildTextMessage('http://tw.iruna-online.info/search?search='.$rplyArr[1]);
