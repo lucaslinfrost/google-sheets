4dfeb1db9a42dbaf4810ce72b48289a6b358881a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-24 00:14:18 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -209,7 +209,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 	       stristr($inputStr, 'Bye') != false||
 	       stristr($inputStr, '再见') != false) {
 	require_once('./authorization.php');
-	if($userId === $owner){ 		
+	//if($userId === $owner){ 		
         if($source['type'] == "room"){  
         $roomId = $source['roomId'];
 	$bot->replyMessage(array(
@@ -230,7 +230,7 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
                 'text' => '｡ﾟヽ(ﾟ´Д`)ﾉﾟ｡有緣再見。'))));
             return $bot->leaveGroup($groupId);
         }
-	}else{return buildTextMessage('你並沒有權限使用這個指令。');}	
+	//}else{return buildTextMessage('你並沒有權限使用這個指令。');}	
 	}
 
 	//公告
