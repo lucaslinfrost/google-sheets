63859c2c25cad6329f7b9a5f0a3f9127a0d386b2
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-26 23:38:45 +0800

    Update sign.php

--- Boss/LINE/sign.php
+++ Boss/LINE/sign.php
@@ -14,17 +14,17 @@ switch ($event['type']) {
         $code = explode(' ', $message['text']);
         $snum = '999';
         if (strpos($code[1], '白羊') !== false||strpos($code[1], '牡羊') !== false||strpos($code[1], '牧羊') !== false||strpos($code[1], '♈') !== false){$snum = 'Aries';$mainname = '牡羊座';$showpic = 'https://i.imgur.com/HjKDnWp.png';}
-        if (strpos($code[1], '金牛') !== false||strpos($code[1], '牡牛') !== false||strpos($code[1], '牧牛') !== false||strpos($code[1], '♉') !== false){$snum = 'Taurus';$mainname = '金牛座';$showpic = '-9003a9c75b4c798997631bf1a868664017b3e0ebd01f0122d8edef1f0f3b9093.png';}
-        if (strpos($code[1], '雙子') !== false||strpos($code[1], '♊') !== false){$snum = 'Gemini';$mainname = '雙子座';$showpic = '-6211c880788e0fa0aac82033a1670f305a0355568350ecdeb118dd1fa5eca80b.png';}
-        if (strpos($code[1], '巨蟹') !== false||strpos($code[1], '♋') !== false){$snum = 'Cancer';$mainname = '巨蟹座';$showpic = '-35feaccf17b943f7982dfcb38c53888d97c7fe212a877844be566f966ccb841f.png';}
-        if (strpos($code[1], '獅子') !== false||strpos($code[1], '♌') !== false){$snum = 'Leo';$mainname = '獅子座';$showpic = '-df7b9fbbe1b7e33c0b666b8e0e8e8a79acf11761f53b16f1bc039e01261ad023.png';}
-        if (strpos($code[1], '處女') !== false||strpos($code[1], '♍') !== false){$snum = 'Virgo';$mainname = '處女座';$showpic = '-315cc8ed1161a91b053ae430c11f6ac341f34793d406143a1eb3c95607776fa3.png';}
-        if (strpos($code[1], '天秤') !== false||strpos($code[1], '天平') !== false||strpos($code[1], '♎') !== false){$snum = 'Libra';$mainname = '天秤座';$showpic = '-2e32d580e9b04ca270d28aaed2ed9257d0110af6fcec82e519767e9cd8047ef9.png';}
-        if (strpos($code[1], '天蠍') !== false||strpos($code[1], '♏') !== false){$snum = 'Scorpio';$mainname = '天蠍座';$showpic = '-abf39b594e52293130d4624f44d3780991e10ca22ddddde8ea24b830f2fb55d2.png';}
-        if (strpos($code[1], '射手') !== false||strpos($code[1], '♐') !== false){$snum = 'Sagittarius';$mainname = '射手座';$showpic = '-9b9c53a96f77ddb95d44fc9e8cd2409a897bc3ba7cbb0be26f9d32313628f43d.png';}
-        if (strpos($code[1], '魔羯') !== false||strpos($code[1], '山羊') !== false||strpos($code[1], '摩羯') !== false||strpos($code[1], '♑') !== false){$snum = 'Capricorn';$mainname = '魔羯座';$showpic = '-c275f6cb9d674a336c3619c1a8761c85cb96d7597dc75eb5a63567005bef8dd9.png';}
-        if (strpos($code[1], '水瓶') !== false||strpos($code[1], '寶瓶') !== false||strpos($code[1], '♒') !== false){$snum = 'Aquarius';$mainname = '水瓶座';$showpic = '-bb68d292e4af2b3f97b2e58fa1d07a80a5dbb87dad7742f8f488b2a5d10db048.png';}
-        if (strpos($code[1], '雙魚') !== false||strpos($code[1], '♓') !== false){$snum = 'Pisces';$mainname = '雙魚座';$showpic = '-9496c34f5fa73dce4c15dff2e99cfd6bcbf283bd4e44c724989d116fe917f582.png';}
+        if (strpos($code[1], '金牛') !== false||strpos($code[1], '牡牛') !== false||strpos($code[1], '牧牛') !== false||strpos($code[1], '♉') !== false){$snum = 'Taurus';$mainname = '金牛座';$showpic = 'https://i.imgur.com/Y9aQpKc.png';}
+        if (strpos($code[1], '雙子') !== false||strpos($code[1], '♊') !== false){$snum = 'Gemini';$mainname = '雙子座';$showpic = 'https://i.imgur.com/OrwPHmq.png';}
+        if (strpos($code[1], '巨蟹') !== false||strpos($code[1], '♋') !== false){$snum = 'Cancer';$mainname = '巨蟹座';$showpic = 'https://i.imgur.com/DNXkfLp.png';}
+        if (strpos($code[1], '獅子') !== false||strpos($code[1], '♌') !== false){$snum = 'Leo';$mainname = '獅子座';$showpic = 'https://i.imgur.com/5pSq4HZ.png';}
+        if (strpos($code[1], '處女') !== false||strpos($code[1], '♍') !== false){$snum = 'Virgo';$mainname = '處女座';$showpic = 'https://i.imgur.com/4CRA6ZY.png';}
+        if (strpos($code[1], '天秤') !== false||strpos($code[1], '天平') !== false||strpos($code[1], '♎') !== false){$snum = 'Libra';$mainname = '天秤座';$showpic = 'https://i.imgur.com/1ZqjWfN.png';}
+        if (strpos($code[1], '天蠍') !== false||strpos($code[1], '♏') !== false){$snum = 'Scorpio';$mainname = '天蠍座';$showpic = 'https://i.imgur.com/AWXaKZs.png';}
+        if (strpos($code[1], '射手') !== false||strpos($code[1], '♐') !== false){$snum = 'Sagittarius';$mainname = '射手座';$showpic = 'https://i.imgur.com/2QcygQx.png';}
+        if (strpos($code[1], '魔羯') !== false||strpos($code[1], '山羊') !== false||strpos($code[1], '摩羯') !== false||strpos($code[1], '♑') !== false){$snum = 'Capricorn';$mainname = '魔羯座';$showpic = 'https://i.imgur.com/lOpNv80.png';}
+        if (strpos($code[1], '水瓶') !== false||strpos($code[1], '寶瓶') !== false||strpos($code[1], '♒') !== false){$snum = 'Aquarius';$mainname = '水瓶座';$showpic = 'https://i.imgur.com/ypqkWlz.png';}
+        if (strpos($code[1], '雙魚') !== false||strpos($code[1], '♓') !== false){$snum = 'Pisces';$mainname = '雙魚座';$showpic = 'https://i.imgur.com/YG5jMGP.png';}
         
         $url = 'https://www.daily-zodiac.com/zodiac/'.$snum;
         $ch = curl_init();
