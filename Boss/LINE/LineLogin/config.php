d318ee0c6dd1bfaef3e598f63036fe830e455e88
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-29 00:10:21 +0800

    Update config.php

--- Boss/LINE/LineLogin/config.php
+++ Boss/LINE/LineLogin/config.php
@@ -5,5 +5,5 @@ $channelSecret = getenv('LINE_CHANNEL_SECRET');
 $channelID = getenv('LINE_Channel_ID');
 define("CLIENT_ID", $channelID);
 define("CLIENT_SECRET", $channelSecret);
-define("REDIRECT_URI", 'https://irunamuscelboss.herokuapp.com/Boss/LINE/LineLogin/callback.php');//登入後返回位置
+define("REDIRECT_URI", 'https://irunamuscelboss.herokuapp.com/Boss/LINE/');//登入後返回位置LineLogin/callback.php
 define("SCOPE", 'openid%20profile%20email');//授權範圍以%20分隔 可以有3項openid，profile，email
