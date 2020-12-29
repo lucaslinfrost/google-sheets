68f4518ea3dad9a16c5b21315f4bd4ca2b1b4b98
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-30 02:20:36 +0800

    Delete config.php

--- Boss/LINE/LineLogin/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-//設定值
-define("CLIENT_ID", '1655532339');
-define("CLIENT_SECRET", '71c74ac4268b9bc1e83809a4826ed26c');
-define("REDIRECT_URI", 'https://liff.line.me/1655532339-AmKmzwND');//登入後返回位置
-define("SCOPE", 'openid%20profile%20email');//授權範圍以%20分隔 可以有3項openid，profile，email
