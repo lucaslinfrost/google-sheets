db00d3c195c318de0b0c5b80ef1926f4e6dd1e7d
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-31 00:36:36 +0800

    Update public.php

--- Boss/LINE/public.php
+++ Boss/LINE/public.php
@@ -44,19 +44,18 @@ $data999 = "您所在的群組還沒有公告，
 		{
 
 	}
-	break;
-	default:
-	break;
+
 }else{
 
 	if($item['gsx$groupid']['$t'] === ""){
+		
 		if($item['gsx$history']['$t'] === ""){
 		$a = "";
 		}else{
 $a = "
 --------活動歷史--------
 ".$item['gsx$history']['$t'];
-}
+		}
 
 	$data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."".$a."\n\n---------發佈者---------\n".$item['gsx$name']['$t']."\n--------發佈時間--------\n".$item['gsx$date']['$t'];
 
