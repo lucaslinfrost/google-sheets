539c87b8c862c55edc5b7c72b2e99a0a9034980a
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-31 05:53:33 +0800

    Update nomalReply.php

--- Boss/LINE/nomalReply.php
+++ Boss/LINE/nomalReply.php
@@ -263,7 +263,8 @@ function KeyWordReply($inputStr,$keyWord,$manualUrl,$textReplyUrl,$userName) {
 	require_once('./public.php');
 	if ($data999 !== "") {
 	//return buildTextMessage($data999);
-	return buildflex($altText, $data999);
+	array_push($result, $data999);
+	return buildflex($altText, $result);
 	}
 	if ($data999 === "") {
 		if($source['type'] == "group"){
