19054d93d93e662c230e9e417164ebf137843f94
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2020-12-31 00:14:00 +0800

    Update public.php

--- Boss/LINE/public.php
+++ Boss/LINE/public.php
@@ -31,39 +31,22 @@ switch ($event['type']) {
                 if($code[1] === Null) {$code[1] = "公告";}
                 if (strcmp($code[1], $keyword) === 0) {
                     
-if($source['type'] == "group"){    
+if($source['type'] == "group"{
 
-if (strcmp($groupId, $groupcheck) === 0) {
-
-$data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."\n\n---------發佈者---------\n".$item['gsx$name']['$t']."\n--------發佈時間--------\n".$item['gsx$date']['$t'];
-}else{
- 
-
-if($code[1] === "公告"){
-    
 $data999 = "您所在的群組還沒有公告，
 可以使用[老大note#公告內容]
 這個指令來添加公告。";
-    
-}else{
 
-if($item['gsx$history']['$t'] === ""){
-$a = "";
-}else{
-$a = "
---------活動歷史--------
-".$item['gsx$history']['$t'];
+foreach ($grouplist as $groupcheck) {
+
+if (strcmp($groupId, $groupcheck) === 0) { 
+$data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."\n\n---------發佈者---------\n".$item['gsx$name']['$t']."\n--------發佈時間--------\n".$item['gsx$date']['$t'];
 }
-$data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."".$a."\n\n---------發佈者---------\n".$item['gsx$name']['$t']."\n--------發佈時間--------\n".$item['gsx$date']['$t'];
-    
-}
-    
+break;
 }
 
 }else{
-
 if($item['gsx$groupid']['$t'] === ""){
-    
 if($item['gsx$history']['$t'] === ""){
 $a = "";
 }else{
@@ -71,12 +54,9 @@ $a = "
 --------活動歷史--------
 ".$item['gsx$history']['$t'];
 }
+
 $data999 = "╭☆╭╧╮╭╧╮╭╧╮\n╰╮║公║║告║║欄║\n☆╰╘∞╛╘∞╛╘∞╛\n\n".$item['gsx$message']['$t']."".$a."\n\n---------發佈者---------\n".$item['gsx$name']['$t']."\n--------發佈時間--------\n".$item['gsx$date']['$t'];
-
-}else{
-
 }
-
 }
                }
             }
