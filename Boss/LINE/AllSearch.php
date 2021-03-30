d0266de8ea5ce99801a216a7082aec0b94cd8eac
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2021-03-31 05:35:46 +0800

    Update AllSearch.php

--- Boss/LINE/AllSearch.php
+++ Boss/LINE/AllSearch.php
@@ -23,6 +23,9 @@ foreach ($client->parseEvents() as $event) {
             $result = array();
             $altText = "關於 ".$message['text']." 的資料";
             $candidate = array('type' => 'bubble','size' => 'micro','hero' => array('type' => 'image','url' => 'https://imgur.com/KQsuipD.png','size' => 'full','aspectMode' => 'fit','aspectRatio' => '320:213'),'body' => array('type' => 'box','layout' => 'vertical','contents' => array(array('type' => 'text','text' => $TitleName,'weight' => 'bold','size' => 'sm','wrap' => true,'align' => 'center'),array('type' => 'box','layout' => 'baseline','contents' => array(array('type' => 'text','text' => '--------------------','size' => 'xs','color' => '#8c8c8c','margin' => 'md','align' => 'center'))),array('type' => 'box','layout' => 'vertical','contents' => array(array('type' => 'box','layout' => 'baseline','spacing' => 'sm','contents' => array(array('type' => 'text','text' => $RealName,'wrap' => true,'color' => '#252dba','size' => 'sm','align' => 'center','action' => array('type' => 'message','label' => 'action','text' => $TextCode,))))))),'spacing' => 'sm','paddingAll' => '13px','backgroundColor' => '#edece8','justifyContent' => 'space-evenly'));
+            $TitleName = "";
+            $RealName = "";
+            $TextCode = "";
             
 //怪物關鍵字
 if ($countno === 0) {
@@ -52,7 +55,11 @@ foreach ($data1['feed']['entry'] as $item) {
 $keywords = explode('、', $item['gsx$key']['$t']);
 foreach ($keywords as $keyword) {
 if (strcmp($code[1], $keyword) === 0) {
-$b = "[搜尋物品] d指令\n";
+$TitleName = "掉落資訊";
+$RealName = $code[1];
+$TextCode = "老大D ".$RealName;
+$candidate = $candidate;
+array_push($result, $candidate);
 unset($json1, $data1, $keywords, $keyword);
 }
 }
