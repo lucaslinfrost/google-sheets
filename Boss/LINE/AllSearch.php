a0dd48f12c8e4c787e78dff0a238f76cf786a1fe
lucaslinfrost <38964564+lucaslinfrost@users.noreply.github.com>
2021-05-27 03:19:13 +0800

    Update AllSearch.php

--- Boss/LINE/AllSearch.php
+++ Boss/LINE/AllSearch.php
@@ -289,7 +289,7 @@ $keywords = explode(',', $item['gsx$key']['$t']);
 foreach ($keywords as $keyword) {
 if (strcmp($code[1], $keyword) === 0) {
 if (strcmp($mapswitch, "off") === 0) {
-$titlename = "地圖導航";
+$titlename = "導航(默認洛庫為起點)";
 $realname = $item['gsx$mapn']['$t'];
 $textcode = "老大導航#".$code[1];
 $candidate = array('type' => 'bubble','size' => 'micro','hero' => array('type' => 'image','url' => 'https://i.imgur.com/CYqZbHw.png','size' => 'full','aspectMode' => 'fit','aspectRatio' => '320:213'),'body' => array('type' => 'box','layout' => 'vertical','contents' => array(array('type' => 'text','text' => $titlename,'weight' => 'bold','size' => 'sm','wrap' => true,'align' => 'center'),array('type' => 'box','layout' => 'baseline','contents' => array(array('type' => 'text','text' => "---------------",'size' => 'xs','color' => '#8c8c8c','margin' => 'md','align' => 'center'))),array('type' => 'box','layout' => 'vertical','contents' => array(array('type' => 'box','layout' => 'baseline','spacing' => 'sm','contents' => array(array('type' => 'text','text' => $realname,'wrap' => true,'color' => '#252dba','size' => 'sm','align' => 'center','action' => array('type' => 'message','label' => 'action','text' => $textcode,))))))),'spacing' => 'sm','paddingAll' => '13px','backgroundColor' => '#edece8','justifyContent' => 'space-evenly'));
